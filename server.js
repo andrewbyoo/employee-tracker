9a1d9349ab2f5d807f475f067eb7db1fff79f4c2
Andrew Yoo <andrewbyoo@gmail.com>
2022-01-17 11:33:21 -0500

    Added express and mysql2 node modules

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
+const express = require('express');
+const mysql = require('mysql2');
 const inquirer = require('inqurier');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
 const employeeTracker = () => {
   inquirer
