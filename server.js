fdef938ec5210447c2ada56afa5b8b131e494d75
Andrew Yoo <andrewbyoo@gmail.com>
2022-01-17 12:49:29 -0500

    Added console.table module

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
+const cTable = require('console.table');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
