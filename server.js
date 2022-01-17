7170895745604ecbd8c430c5078041c639fdab72
Andrew Yoo <andrewbyoo@gmail.com>
2022-01-17 11:03:50 -0500

    Added inquirer require and function with the general menu when starting the app

---
+++ server.js
@@ -0,0 +1,16 @@
+const inquirer = require('inqurier');
+
+const employeeTracker = () => {
+  inquirer
+    .prompt(
+      {
+        type: 'list',
+        name: 'generalMenu',
+        message: 'What would you like to do?',
+        choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department']
+      }
+    )
+};
+
+// Initialize app
+employeeTracker();
