ceb692fc9019f3b0876ce756a157adbccb5e1dba
Andrew Yoo <andrewbyoo@gmail.com>
2022-01-17 14:36:12 -0500

    Removed console.table module as node includes the console.table method by default

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
-const cTable = require('console.table');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
