fada006c1fd7391cd1f5ecac99867924ba1a12cf
Andrew Yoo <andrewbyoo@gmail.com>
2022-01-17 15:00:58 -0500

    Fixed foreign key and added primary key to auto increment in employees table

--- db/schema.sql
+++ db/schema.sql
@@ -12,16 +12,18 @@ CREATE TABLE roles (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL NOT NULL,
-  department_id INT FOREIGN KEY
+  department_id INT,
+  FOREIGN KEY (department_id)
   REFERENCES departments(id)
   ON DELETE SET NULL
 );
 
 CREATE TABLE employees (
-  id INT NOT NULL AUTO_INCREMENT,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  role_id INT FOREIGN KEY
+  role_id INT,
+  FOREIGN KEY (role_id)
   REFERENCES roles(id)
   ON DELETE SET NULL,
   manager_id INT -- should be whatever the manager "employee" id number is, possibly add inquirer prompt with list of available managers to connect to employee?
