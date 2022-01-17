f5ceeabb9337295c62ca27f2afac48b1fb7bc945
Andrew Yoo <andrewbyoo@gmail.com>
2022-01-17 12:53:04 -0500

    Added drop, create, and use for both departments and roles

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,14 @@
+DROP DATABASE IF EXISTS departmentList_db;
+CREATE DATABASE departmentList_db;
+
+USE departmentList_db
+
+DROP DATABASE IF EXISTS roleList_db;
+CREATE DATABASE roleList_db;
+
+USE roleList_db;
+
 DROP DATABASE IF EXISTS employeeList_db;
 CREATE DATABASE employeeList_db;
 
-use employeeList_db;
+USE employeeList_db;
