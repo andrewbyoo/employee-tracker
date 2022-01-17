fa8f35bbb5a48ca9e89409e280c6d66efce72a31
Andrew Yoo <andrewbyoo@gmail.com>
2022-01-17 11:26:24 -0500

    Added blank switch cases for each inquirer option

--- server.js
+++ server.js
@@ -10,6 +10,24 @@ const employeeTracker = () => {
         choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department']
       }
     )
+    .then(function (response) {
+      switch (response) {
+        case 'View All Employees':
+          break;
+        case 'Add Employee':
+          break;
+        case 'Update Employee Role':
+          break;
+        case 'View All Roles':
+          break;
+        case 'Add Role':
+          break;
+        case 'View All Departments':
+          break;
+        case 'Add Department':
+          break;
+      }
+    })
 };
 
 // Initialize app
