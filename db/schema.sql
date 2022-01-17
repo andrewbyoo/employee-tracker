df85686bf9debbf3cc4f40cf6ebf1e671ea5f3a1
Andrew Yoo <andrewbyoo@gmail.com>
2022-01-17 12:58:13 -0500

    Added department list create table

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,12 @@
 DROP DATABASE IF EXISTS departmentList_db;
 CREATE DATABASE departmentList_db;
 
-USE departmentList_db
+USE departmentList_db;
+
+CREATE TABLE departments (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
+);
 
 DROP DATABASE IF EXISTS roleList_db;
 CREATE DATABASE roleList_db;
