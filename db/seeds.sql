08299866c7de5e7521cc0f545cbe0f1cf1550637
Andrew Yoo <andrewbyoo@gmail.com>
2022-01-17 18:15:49 -0500

    Added values for roles

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,3 +3,13 @@ VALUES ('Sales'),
        ('Engineering'),
        ('Finance'),
        ('Legal');
+
+INSERT INTO roles (title, department_id, salary)
+VALUES ('Sales Lead', 1, 100000),
+       ('Salesperson', 1, 80000),
+       ('Lead Engineer', 2, 150000),
+       ('Software Engineer', 2, 120000),
+       ('Account Manager', 3, 160000),
+       ('Accountant', 3, 125000),
+       ('Legal Team Lead', 4, 250000),
+       ('Lawyer', 4, 190000);
