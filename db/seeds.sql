3d33097046e5a3a4887c6d25f29b201dda5834ec
Andrew Yoo <andrewbyoo@gmail.com>
2022-01-17 19:15:34 -0500

    Added values for employees

--- db/seeds.sql
+++ db/seeds.sql
@@ -13,3 +13,13 @@ VALUES ('Sales Lead', 1, 100000),
        ('Accountant', 3, 125000),
        ('Legal Team Lead', 4, 250000),
        ('Lawyer', 4, 190000);
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES ('Jane', 'Doe', 1, NULL),
+       ('Frances', 'Bolton', 2, 1),
+       ('Marie', 'Maurer', 3, NULL),
+       ('Barbara', 'Carter', 4, 3),
+       ('Sally', 'Wilson', 5, NULL),
+       ('Kareem', 'Saba', 6, 5),
+       ('Martha', 'Worm', 7, NULL),
+       ('Charles', 'Holland', 8, 7);
